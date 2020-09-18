d218a2e9a02ac79a9305919c425b13c6ae6bb2de
Itay <itag7894@gmail.com>
2020-09-18 19:09:45 +0300

    first commit

---
+++ AntiCheatScoreCalculator/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace AntiCheatScoreCalculator
+{
+    partial class AntiCheatScoreCalculator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AntiCheatScoreCalculator));
+            this.RunButton = new System.Windows.Forms.Button();
+            this.RatingAverageTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.OutPutTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RatingCountTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // RunButton
+            // 
+            this.RunButton.Location = new System.Drawing.Point(85, 185);
+            this.RunButton.Name = "RunButton";
+            this.RunButton.Size = new System.Drawing.Size(199, 30);
+            this.RunButton.TabIndex = 0;
+            this.RunButton.Text = "Run";
+            this.RunButton.UseVisualStyleBackColor = true;
+            this.RunButton.Click += new System.EventHandler(this.RunButton_Click);
+            // 
+            // RatingAverageTxt
+            // 
+            this.RatingAverageTxt.Location = new System.Drawing.Point(85, 72);
+            this.RatingAverageTxt.Name = "RatingAverageTxt";
+            this.RatingAverageTxt.Size = new System.Drawing.Size(98, 20);
+            this.RatingAverageTxt.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(40, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 29);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "AntiCheat Score Calculator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(85, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Rating average:";
+            // 
+            // OutPutTxt
+            // 
+            this.OutPutTxt.Location = new System.Drawing.Point(85, 125);
+            this.OutPutTxt.Name = "OutPutTxt";
+            this.OutPutTxt.Size = new System.Drawing.Size(199, 20);
+            this.OutPutTxt.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(82, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "output:";
+            // 
+            // RatingCountTxt
+            // 
+            this.RatingCountTxt.Location = new System.Drawing.Point(190, 73);
+            this.RatingCountTxt.Name = "RatingCountTxt";
+            this.RatingCountTxt.Size = new System.Drawing.Size(94, 20);
+            this.RatingCountTxt.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(190, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Rating count:";
+            // 
+            // AntiCheatScoreCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(380, 227);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.RatingCountTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.OutPutTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.RatingAverageTxt);
+            this.Controls.Add(this.RunButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AntiCheatScoreCalculator";
+            this.Text = "AntiCheatScoreCalculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button RunButton;
+        private System.Windows.Forms.TextBox RatingAverageTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox OutPutTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox RatingCountTxt;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ AntiCheatScoreCalculator/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntiCheatScoreCalculator
+{
+    public partial class AntiCheatScoreCalculator : Form
+    {
+        public AntiCheatScoreCalculator()
+        {
+            InitializeComponent();
+        }
+
+        private void RunButton_Click(object sender, EventArgs e)
+        {
+            if (RatingCountTxt.Text.Length == 0 || RatingAverageTxt.Text.Length == 0)
+            {
+                MessageBox.Show("Please fill all fields", "Error");
+                return;
+            }
+            else
+            {
+                if (ContainsNotDigits(0, RatingCountTxt.Text) || ContainsNotDigits(0, RatingAverageTxt.Text))
+                {
+                    MessageBox.Show("Wrong arguments, please write numbers only", "Error");
+                    return;
+                }
+            }
+            OutPutTxt.Text = "" + (10 * 3 + float.Parse(RatingAverageTxt.Text) * int.Parse(RatingCountTxt.Text)) / (10 + int.Parse(RatingCountTxt.Text));
+        }
+
+        private bool ContainsNotDigits(int i, String txt)
+        {
+            if (txt.Length == i) return false;
+            if (!Char.IsDigit(txt[i]) && txt[i] != '.') return true;
+            return ContainsNotDigits(i+=1, txt);
+        }
+    }
+}
